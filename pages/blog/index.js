1715105e154c301ba2bb467a3d719daf891ea80d
itc-s23015 <s23015@std.it-college.ac.jp>
2024-01-25 07:13:43 +0000

    chap4finish

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,11 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 const Blog = () => {
   return (
-    <Hero
-      title='Blog'
-      subtitle='Recent Posts'
-    />
+    <Container>
+      <Hero title='Blog' subtitle='Recent Posts' />
+    </Container>
   )
 }
 export default Blog
