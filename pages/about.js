0be327c4703ac153a01bc3f561ce3b6e10e35b4f
itc-s23015 <s23015@std.it-college.ac.jp>
2024-02-05 01:25:29 +0000

    chapter6finish

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
@@ -13,6 +14,14 @@ import eyecatch from 'images/about.jpg'
 const About = () => {
   return (
     <Container>
+      <Meta
+        pageTitle='アバウト'
+        pageDesc='About development activities'
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+
       <Hero title='About' subtitle='About development activities' />
 
       <figure>
