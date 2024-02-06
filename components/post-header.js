364cb837ca0797ccaf642216ff6b44c0891a8719
itc-s23015 <s23015@std.it-college.ac.jp>
2024-02-06 04:41:04 +0000

    コードミス修正

--- components/post-header.js
+++ components/post-header.js
@@ -7,7 +7,7 @@ const PostHeader = ({ title, subtitle, publish = '' }) => {
   return (
     <div className={styles.stack}>
       <p className={styles.subtitle}>{subtitle}</p>
-      <h1 classname={styles.title}>{title}</h1>
+      <h1 className={styles.title}>{title}</h1>
       {publish && (
         <div className={styles.publish}>
           <FontAwesomeIcon icon={faClock} size='lg' color='var(--gray-25)' />
